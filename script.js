73ad4a6c598c4701a64a455e14ca871d544715a7
gabrimx7 <121612254+GabriMX7@users.noreply.github.com>
2024-12-30 09:11:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-const countdownElement = document.getElementById('countdown');
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
-// Defina a data e hora de término da manutenção (ajuste conforme necessário)
-const countDownDate = new Date("Seg, 30 00:00:59").getTime();
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
 
-// Atualiza o contador a cada segundo
-let x = setInterval(function() {
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
 
-  // Obter a data e hora atuais
-  const now = new Date().getTime();
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
 
-  // Encontrar a distância entre a data de contagem regressiva e agora
-  const distance = countDownDate - now;
+setInterval(setDate, 1000);
 
-  // Cálculos para dias, horas, minutos e segundos
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Exibir o resultado em um elemento HTML
-  countdownElement.innerHTML = days + "31" + hours + "00"
-  + minutes + "00" + seconds + "59";
-
-  // Se a contagem regressiva tiver terminado, escreva algum texto
-  if (distance < 0) {
-    clearInterval(x);
-    countdownElement.innerHTML = "Estamos de volta!";
-  }
-}, 1000);
+setDate();
